95a97de6fd0b0a06607fc5b7510c5a21a785c522
zeemyself <zeemyself@gmail.com>
2017-10-05 01:24:52 +0700

    Add agrument

--- tennis.py
+++ tennis.py
@@ -1,48 +1,54 @@
-# Read input.txt file
-# f = open("input.txt","r")
+import sys
+# import argparse
 #
-# # print(f.readline())
-# # print(f.readline())
-# # print(f.readline())
-# # print(f.readline())
-#
-# input = f.readline()
-# # while input != '':
-# #     print(input)
+# parser = argparse.ArgumentParser()
+# parser.add_argument("text", default="input.txt", action='store_true')
+# t = parser.parse_args().text
+# print(t)
+# print(t is bool)
+# print(isinstance(t, bool))
+if len(sys.argv) > 1:
+    input = sys.argv[1]
+else:
+    input = "input.txt"
+
 scoreArr = [0, 15, 30, 40]
 
 def score(a, b, win):
     if a > 3 and a - b >= 2:
         print('A: WIN')
+        return True
     elif b > 3 and b - a >=2:
         print('B: WIN')
+        return True
     elif a > 3 and b> 3 and a==b:
         print(win + ': ADV')
         print('DUCE')
     elif a >= 4 and win == 'A':
         print('A: ADV')
     elif b >= 4 and win =='B':
-        print('B: ADB')
+        print('B: ADV')
     elif win == 'A':
         print('A: ' + str(scoreArr[a]))
-        if a==b:
-            print('DUCE')
     elif win == 'B':
         print('B: ' + str(scoreArr[b]))
     if a == b == 3:
-        print('DUCE +++ ')
+        print('DUCE')
 
-
-with open('input.txt', 'r') as input:
-    for games in input:
-        a = b = 0
-        game = games.rstrip().split(":")
-        print(game[0]) # Game X
-        win = game[1].split(" ")
-        for winner in win:
-            if winner == 'A':
-                a = a + 1
-                score(a, b, winner)
-            elif winner == 'B':
-                b = b + 1
-                score(a, b, winner)
+try:
+    with open(input, 'r') as input:
+        for games in input:
+            a = b = 0
+            game = games.rstrip().split(":")
+            print(game[0]) # Game X
+            win = game[1].split(" ")
+            for winner in win:
+                if winner == 'A':
+                    a = a + 1
+                elif winner == 'B':
+                    b = b + 1
+                if(score(a, b, winner)):
+                    break
+            print('\n')
+except IOError:
+    print("Error %s file not found." % input)
